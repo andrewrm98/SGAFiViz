b877c53b99b22741e7c39b50be225249164456eb
Andrew <andrewrm98@gmail.com>
2020-03-23 17:17:50 -0400

    started on radar

---
+++ fi-viz/src/pages/budget/radarChart.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import updateRadar from './radarChart.js';
+import * as d3 from 'd3'
+import data from './FinalDataVis.csv'
+
+class RadarChart extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      d3: '',
+      height: 0,
+      width: 0
+    }
+    this.chartRef = React.createRef();
+  }
+
+  componentDidMount() {
+    updateRadar(data, 'ME') // looping a bunch of times?
+  }
+  componentDidUpdate() {
+    //updateRadar('ME');
+  }
+
+  render() { return <div id="radar"></div> }
+}
+
+
+export default RadarChart;
